1bb95a75f27897137983cb2ac0d026350d573baa
Isman Firmansyah <isman.firmansyah@gmail.com>
2012-08-11 16:54:59 +0700

    installer and uninstaller scripts

---
+++ dotfiles-install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Directory of this script, see http://is.gd/lUsWul
+SOURCE="${BASH_SOURCE[0]}"
+DIR=$( dirname "$SOURCE" )
+
+while [ -h "$SOURCE" ]; do
+    SOURCE=$(readlink "$SOURCE")
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    DIR=$( cd -P "$( dirname "$SOURCE"  )" && pwd )
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" && pwd )
+
+# Find files prefixed with underscore
+FILES=$( find $DIR -maxdepth 1 -name '_*')
+
+for file in $FILES; do
+    dotfile=${file/$DIR\/_/$HOME\/.}
+    echo "symlinking ${file} to ${dotfile}"
+    [[ -e $dotfile ]] && rm $dotfile
+    ln -s $file $dotfile -f
+done
---
+++ dotfiles-remove.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Directory of this script, see http://is.gd/lUsWul
+SOURCE="${BASH_SOURCE[0]}"
+DIR=$( dirname "$SOURCE" )
+
+while [ -h "$SOURCE" ]; do
+    SOURCE=$(readlink "$SOURCE")
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    DIR=$( cd -P "$( dirname "$SOURCE"  )" && pwd )
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" && pwd )
+
+# Find files prefixed with underscore
+FILES=$( find $DIR -maxdepth 1 -name '_*')
+
+for file in $FILES; do
+    dotfile=${file/$DIR\/_/$HOME\/.}
+    echo "unlinking ${dotfile} from ${file}"
+    rm -rf $dotfile
+done
