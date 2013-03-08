1723f55f26aa1a0eb987804104aa1f9e8fbf1213
Isman Firmansyah <isman.firmansyah@gmail.com>
2013-03-08 10:51:36 +0700

    moar

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,8 @@
-from fabric.api import local
 from os import path as op
 from os import getenv as _getenv
 from os import listdir as _listdir
+
+from fabric.api import local
 
 
 _src_dir = op.abspath(op.dirname(__file__))
@@ -9,6 +10,8 @@ _dst_dir = _getenv('HOME')
 
 
 def install():
+    """Installs dotfiles to $HOME directory.
+    """
     src = [f for f in _listdir(_src_dir) if f.startswith('_')]
     for df in src:
         source = op.join(_src_dir, df)
@@ -19,6 +22,9 @@ def install():
 
 
 def remove():
+    """Uninstalls dotfiles from $HOME directory.
+    """
+    src = [f for f in _listdir(_src_dir) if f.startswith('_')]
     src = [f for f in _listdir(_src_dir) if f.startswith('_')]
     for df in src:
         target = op.join(_dst_dir, df.replace('_', '.', 1))
@@ -27,4 +33,6 @@ def remove():
 
 
 def update_submodules():
+    """Updates all registered submodules (requires git).
+    """
     local('git submodule foreach "git pull"')
